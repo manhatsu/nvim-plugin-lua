119916c32b3e849f95b3b324485e4789af9b3a7b
manhatsu <manhatsu@gmail.com>
2024-09-26 23:02:59 +0900

    feat: add plenary test function

--- init.lua
+++ init.lua
@@ -1,9 +1,3 @@
 require('greeting')
 require('openbuffer')
 require('tests')
-
-vim.opt.rtp:prepend(vim.env.HOME .. '/.local/share/nvim/lazy/lazy.nvim')
-
-require('lazy').setup({
-  { 'nvim-lua/plenary.nvim' },
-})
--- lua/tests/init.lua
+++ lua/tests/init.lua
@@ -1,2 +1,11 @@
-require('tests.test_verticalbuffer')
-vim.opt.rtp:prepend(vim.env.HOME .. '/.local/share/nvim/lazy/plenary.nvim')
+local plenary_dir = os.getenv("PLENARY_DIR") or "/tmp/plenary.nvim"
+local is_not_a_directory = vim.fn.isdirectory(plenary_dir) == 0
+if is_not_a_directory then
+  vim.fn.system({"git", "clone", "https://github.com/nvim-lua/plenary.nvim", plenary_dir})
+end
+
+vim.opt.rtp:append(".")
+vim.opt.rtp:append(plenary_dir)
+
+vim.cmd("runtime plugin/plenary.vim")
+require("plenary.busted")
